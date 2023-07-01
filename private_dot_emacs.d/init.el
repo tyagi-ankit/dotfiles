38bdd33d8dc9615f3dd280a35476ccda4ccd80cf
Ankit Tyagi <civiltyagi@gmail.com>
2023-07-01 12:30:00 +0530

    add init.el

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,10 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; pdf-tools activation
+(pdf-tools-install)  ; Standard activation command
+;; (pdf-loader-install) ; On demand loading, leads to faster startup time
