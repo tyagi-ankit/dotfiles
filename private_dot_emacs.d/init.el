86d3fad954de96734a3181c83933a189c9b13e20
Ankit Tyagi <civiltyagi@gmail.com>
2023-07-01 12:42:14 +0530

    Add capture templates and global keys for link, agenda etc

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -8,3 +8,16 @@
 ;; pdf-tools activation
 (pdf-tools-install)  ; Standard activation command
 ;; (pdf-loader-install) ; On demand loading, leads to faster startup time
+
+;; define global keys for three commands that are useful in any
+;; Emacs buffer, not just Org buffers
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;; Capture templates
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/OneDrive/org/gtd.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "~/OneDrive/org/journal.org")
+         "* %?\nEntered on %U\n  %i\n  %a")))
